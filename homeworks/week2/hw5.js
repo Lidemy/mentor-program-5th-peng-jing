47c1400d4aa449612cb5db69a428e4b48dd69d7e
peng-jing <misjsdes@gmail.com>
2021-04-24 19:14:42 +0800

    week2_4/24

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,19 @@
 function join(arr, concatStr) {
-  
+  var arrJoin=arr[0]
+  for(var i=1; i<arr.length; i++){
+  	arrJoin+= concatStr + arr[i]
+  	
+  }
+  return arrJoin
 }
 
 function repeat(str, times) {
-  
+	var strRep=""
+  	for(var i=0; i<times; i++){
+  		strRep+= str
+  	}
+  	return strRep
 }
 
-console.log(join(['a'], '!'));
+console.log(join(["a",1,"b",2,"c",3], ','));
 console.log(repeat('a', 5));
