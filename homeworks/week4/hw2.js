e357245c51ea6cfbcb6cab58c2c085f489c6a8d3
peng-jing <misjsdes@gmail.com>
2021-05-16 23:03:23 +0800

    week5_5/16

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -2,7 +2,7 @@ const request = require('request')
 const process = require('process')
 let booksUrl = 'https://lidemy-book-store.herokuapp.com/books/'
 let Method = process.argv[2]
-let idName = process.argv[3]
+let id = process.argv[3]
 let Name = process.argv[4]
 // console.log(process.argv)
 resMethod() (
@@ -13,12 +13,23 @@ resMethod() (
 )
 
 function result(){ //印出的內容
-	if(Method === undefined) return booksUrl
-	if(Method === 'list') return booksUrl + '?_limit=20' // 印出前二十本書的 id 與書名
-	if(Method === 'read') return booksUrl + idName // 輸出指定 id 的書籍
-	if(Method === 'delete') return booksUrl + idName // 刪除指定 id 的書籍
-	if(Method === 'create') return Add() // 新增書籍
-	if(Method === 'update') return Modify() //修改書籍名稱
+	switch(Method){
+		case 'list': return booksUrl + '?_limit=20' // 印出前二十本書的 id 與書名
+		case 'read': 
+		case 'delete':
+			return booksUrl + id // 指定 id
+		// case 'delete': return booksUrl + id // 刪除指定 id 的書籍
+		case 'create': return Add(process.argv[3]) // 新增書籍
+		case 'update': return Modify(process.argv[3],process.argv[4]) //修改書籍名稱
+		default: return booksUrl
+	}
+		
+	// if(Method === undefined) return booksUrl
+	// if(Method === 'list') return booksUrl + '?_limit=20' // 印出前二十本書的 id 與書名
+	// if(Method === 'read') return booksUrl + id // 輸出指定 id 的書籍
+	// if(Method === 'delete') return booksUrl + id // 刪除指定 id 的書籍
+	// if(Method === 'create') return Add(process.argv[3]) // 新增書籍
+	// if(Method === 'update') return Modify(process.argv[3],process.argv[4]) //修改書籍名稱
 } 
 function resMethod(){  //判斷執行動作
 	if(Method === 'list' || Method === 'read' || Method === undefined) return request
@@ -26,19 +37,19 @@ function resMethod(){  //判斷執行動作
 	if(Method === 'create') return request.post
 	if(Method === 'update') return request.patch
 }
-function Add(){ // 新增指定名稱的書
+function Add(Name){ // 新增指定名稱的書
 	return {
 		url: booksUrl,
 		form: {
-			name: idName
+			name: Name
 		}
 	}
 }
-function Modify(){ // 修改指定書籍的名稱
+function Modify(id,name){ // 修改指定書籍的名稱
 	return {
-		url: booksUrl+idName,
+		url: booksUrl+id,
 		form: {
-			name: Name
+			name: name
 		}
 	}
 }
